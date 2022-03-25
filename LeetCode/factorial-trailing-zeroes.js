9a83e3fdde3e854285f89f91a507cde3cc4a4d60
Inwell <1079702627@qq.com>
2022-03-25 13:14:39 +0800

    LeetCode-每日 2022-3-25改

--- LeetCode/factorial-trailing-zeroes.js
+++ LeetCode/factorial-trailing-zeroes.js
@@ -5,6 +5,7 @@
 var trailingZeroes = function (n) {
     //尾零的数量是因子2和因子5的数量中较少的那个，又能知道因子5的数量少于因子2。
     //所以题目就等价于求因子5的数量。
+    if (n <= 4) return 0;
     let ans = 0;
     while (n > 0) {
         n = Math.floor(n / 5);
