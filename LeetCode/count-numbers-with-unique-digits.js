d84d295883a6e073f79265b14bb8a7278ee6e3c2
Inwell <1079702627@qq.com>
2022-04-11 22:49:37 +0800

    LeetCode-每日 2022-4-11

---
+++ LeetCode/count-numbers-with-unique-digits.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+ var countNumbersWithUniqueDigits = function(n) {
+    let ans=0;
+    for(let i=0;i<=n;i++){
+        ans+=getNum(i);
+    }
+    return ans
+};
+const getNum=(num)=>{ 
+    if(num===0) return 1;
+    let cnt=9,limit=10-num;
+    for(let i=9;i>limit;i--){
+        cnt*=i;
+    }
+    return cnt;
+}
