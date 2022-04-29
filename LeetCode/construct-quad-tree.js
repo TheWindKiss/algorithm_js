2f8f6ab91e9e4183e3c56d9f8a576af87677dff7
Inwell <1079702627@qq.com>
2022-04-29 18:30:09 +0800

    LeetCode-每日 2022-4-29

---
+++ LeetCode/construct-quad-tree.js
@@ -0,0 +1,47 @@
+/**
+ * // Definition for a QuadTree node.
+ * function Node(val,isLeaf,topLeft,topRight,bottomLeft,bottomRight) {
+ *    this.val = val;
+ *    this.isLeaf = isLeaf;
+ *    this.topLeft = topLeft;
+ *    this.topRight = topRight;
+ *    this.bottomLeft = bottomLeft;
+ *    this.bottomRight = bottomRight;
+ * };
+ */
+
+/**
+ * @param {number[][]} grid
+ * @return {Node}
+ */
+var construct = function (grid) {
+    return dfs(grid, 0, 0, grid.length, grid.length);
+};
+const dfs = (grid, x0, y0, x1, y1) => {
+    let same = true;
+    for (let i = x0; i < x1; ++i) {
+        for (let j = y0; j < y1; ++j) {
+            if (grid[i][j] !== grid[x0][y0]) {
+                same = false;
+                break;
+            }
+        }
+        if (!same) {
+            break;
+        }
+    }
+
+    if (same) {
+        return new Node(grid[x0][y0] === 1, true);
+    }
+
+    const ret = new Node(
+        true,
+        false,
+        dfs(grid, x0, y0, Math.floor((x0 + x1) / 2), Math.floor((y0 + y1) / 2)),
+        dfs(grid, x0, Math.floor((y0 + y1) / 2), Math.floor((x0 + x1) / 2), y1),
+        dfs(grid, Math.floor((x0 + x1) / 2), y0, x1, Math.floor((y0 + y1) / 2)),
+        dfs(grid, Math.floor((x0 + x1) / 2), Math.floor((y0 + y1) / 2), x1, y1)
+    );
+    return ret;
+}
